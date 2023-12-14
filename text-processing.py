33fa8791de97b4940b34cde2bce3655a5f6bbd17
kand7dev <kand7dev@gmail.com>
2023-12-15 00:02:23 +0200

    add tokenizer function
    Use the word_tokenize nltk function.
    
    Make tokens lowercase.
    
    Remove punctuation and stopwords.

---
+++ text-processing.py
@@ -0,0 +1,23 @@
+import string
+from nltk import word_tokenize, corpus, PorterStemmer
+
+
+def abstract_tokenize(abstract):
+    
+    tokens = word_tokenize(abstract) #nltk's tokenizer
+    
+    lowercase_tokens = [char.lower() for char in tokens]
+    
+    punctuation_removed_tokens = list()
+    for token in lowercase_tokens:
+        if token not in string.punctuation:
+            punctuation_removed_tokens.append(token)
+
+    stopwords = corpus.stopwords.words("english")
+    final_tokens = list()
+    for token in punctuation_removed_tokens:
+        if token not in stopwords:
+            final_tokens.append(token)
+    
+    return final_tokens
+
