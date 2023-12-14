6bb86d352090c090bae3c36bd0bfc794022be1b7
kand7dev <kand7dev@gmail.com>
2023-12-15 00:00:25 +0200

    remove todo and add spacing

--- simple-crawler.py
+++ simple-crawler.py
@@ -3,15 +3,6 @@ from bs4 import BeautifulSoup
 import json
 
 
-# TO DO 
-#   1. Scrape Title [X]
-#   2. Scrape Authors [X]
-#   3. Scrape Abstract [X]
-#   4. Scrape Submission Date [X]
-#   5. Convert Scraped Data to Dictionary [X]
-#   6. Convert Dictionary to JSON Object [X]
-
-
 def crawler(url):
     html_respone = requests.get(url)
     if html_respone.ok:
@@ -20,12 +11,17 @@ def crawler(url):
         
         # scraping data
         title = soup.find("p", class_="title is-5 mathjax").text.strip()
+        
         authors = soup.find("p", class_="authors").find_all("a")
+        
         paper_authors_list = list()
         for author in authors:
             paper_authors_list.append(author.text)
+    
         submission = soup.find("p", class_="is-size-7").find("span", class_="has-text-black-bis has-text-weight-semibold").nextSibling.text.strip().removesuffix(";")  
+        
         abstract = soup.find("span", class_="abstract-full has-text-grey-dark mathjax").text.strip().replace("△ Less", "").strip()
+        
         id = 1
         
         # creating dict
