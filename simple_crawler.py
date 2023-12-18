ff841b554c84ebf7d478d1b46945792facfbf8fb
kand7dev <kand7dev@gmail.com>
2023-12-19 01:01:29 +0200

    scrape all documents in a page
    1. Combine our word generator with a find_all function to get all documents.
    2. Parser has been changed to html.parser, usage of lxml was redundant.
    3. Apart from returning the list of .json objects we're also saving them to data.json file.

--- simple_crawler.py
+++ simple_crawler.py
@@ -8,40 +8,48 @@ def crawler():
     random = random_word.RandomWords()
     json_object_list = list()
     id = 1
-    for i in range(1,50):
+    for i in range(10):
+        
         word = random.get_random_word()
-        url = "https://arxiv.org/search/?query="+str(word)+"&searchtype=all&source=header"
+        url = url = "https://arxiv.org/search/?query=" + str(word) + "&searchtype=all&source=header"
         html_respone = requests.get(url)
+        
         if html_respone.ok:
+            
             try:
-                soup = BeautifulSoup(html_respone.content,"lxml") # html_response.content contains the whole html document.
-                
+                soup = BeautifulSoup(html_respone.content,"html.parser") # html_response.content contains the whole html document.
+                    
                 # scraping data
-                title = soup.find("p", class_="title is-5 mathjax").text.strip()
+                html_titles = soup.find_all("p", class_="title is-5 mathjax")
+                html_authors = soup.find_all("p", class_="authors")
+                html_submissions = soup.select("p.is-size-7:not(.comments)")
+                html_abstracts = soup.find_all("span", class_="abstract-full has-text-grey-dark mathjax")
+                authors_list = list() # is used to store all authors, gets re-initialized in each iteration
                 
-                authors = soup.find("p", class_="authors").find_all("a")  
-                paper_authors_list = list()
-                for author in authors:
-                    paper_authors_list.append(author.text)
-            
-                submission = soup.find("p", class_="is-size-7").find("span", class_="has-text-black-bis has-text-weight-semibold").nextSibling.text.strip().removesuffix(";")  
-                
-                abstract = soup.find("span", class_="abstract-full has-text-grey-dark mathjax").text.strip().replace("△ Less", "").strip()
-                tokenized_abstract = text_processing.lemmatizer(text_processing.abstract_tokenize(abstract))
-                
-                # creating dict
-                paper_dict = dict(paper_id = id, paper_title = title, paper_authors = paper_authors_list, paper_abstract = tokenized_abstract, paper_submission_date = submission )
-                
-                # convert dict to JSON
-                json_object = json.dumps(paper_dict)
-                json_object_list.append(json_object)
-                
-                id+=1
-            
+                for i in range(len(html_titles)):
+                    
+                    title = html_titles[i].text.strip()
+                    for author in html_authors[i].find_all("a"):
+                        authors_list.append(author.text)
+                    
+                    submission = html_submissions[i].find("span", class_="has-text-black-bis has-text-weight-semibold").nextSibling.text.strip().removesuffix(";")
+                    abstract = html_abstracts[i].text.strip().replace("△ Less", "").strip()    
+                    tokenized_abstract = text_processing.lemmatizer(text_processing.abstract_tokenize(abstract))
+                    
+                    # creating json object (must be a dictionary to use json.dumps)
+                    paper_dict= dict(paper_id = id, paper_title = title, paper_authors = authors_list, paper_abstract = tokenized_abstract, paper_submission_date = submission )
+                    json_object = json.dumps(paper_dict)
+                    json_object_list.append(json_object)
+                    authors_list.clear()
+                    id+=1
+
             except AttributeError:
                 
                 continue
             
-    return json_object_list
             
-
+    # save data to .json file       
+    with open("data.json","w") as output:
+        json.dump(json_object_list,output)
+    
+    return json_object_list
