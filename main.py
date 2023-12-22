f0471bea0e843b631a0029aafaa4d9a9a4e29b3c
kand7dev <kand7dev@gmail.com>
2023-12-23 00:42:11 +0200

    create main.py as our program entry point

---
+++ main.py
@@ -0,0 +1,14 @@
+import simple_crawler, indexing
+
+
+def main():
+    
+    if simple_crawler.crawler():
+        
+        index = indexing.inverted_index()
+        for key, value in index.items():
+            print(f"{key} found in Documents {value}")
+
+
+if __name__ == "__main__":
+    main()
