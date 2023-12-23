f00d8b5a57fe8af1fb85f1c479f1df23eb623d17
kand7dev <kand7dev@gmail.com>
2023-12-23 02:23:23 +0200

    print record_level_inverted_index and word_level_inverted_index in main function

--- main.py
+++ main.py
@@ -5,9 +5,25 @@ def main():
     
     if simple_crawler.crawler():
         
-        index = indexing.inverted_index()
-        for key, value in index.items():
+        record_level_index = indexing.record_level_inverted_index()
+        for key, value in record_level_index.items():
             print(f" '{key}' found in Documents {value}")
+            
+
+        word_level_index = indexing.word_level_inverted_index()
+        
+        for key, value in word_level_index.items():
+            coordinates, times = value
+            print(f"Key -> {key} , Found At -> {coordinates} , Total Times -> {times}")
+           
+            
+        # Verbose Output (uncomment to see)
+
+        # for key, value in word_level_index.items():
+        #     my_tuple, times = value
+        #     for t in my_tuple:
+        #        print(f"Key -> {key} : Found In Document -> {t[0]} : Found At Index -> {t[1]}")
+        #     print(f"Total Times -> {times}")
 
 
 if __name__ == "__main__":
